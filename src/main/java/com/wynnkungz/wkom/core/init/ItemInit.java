356450e68f5880411f8d07d1330d369b3d37e1df
WynnKunGz <wynn.chanakant@gmail.com>
2021-06-15 22:26:23 +0700

    Update ItemInit.java

--- src/main/java/com/wynnkungz/wkom/core/init/ItemInit.java
+++ src/main/java/com/wynnkungz/wkom/core/init/ItemInit.java
@@ -1,9 +1,13 @@
 package com.wynnkungz.wkom.core.init;
 
 import com.wynnkungz.wkom.WynnkungzOresMod;
+import com.wynnkungz.wkom.common.material.CustomArmorMaterial;
+import com.wynnkungz.wkom.common.material.CustomToolMaterial;
 import com.wynnkungz.wkom.core.itemgroup.WKOMItemGroup;
 
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,22 +18,54 @@ public class ItemInit {
 
 	public static final RegistryObject<Item> COPPER_INGOT = ITEMS.register("copper_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
+
 	public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
+
 	public static final RegistryObject<Item> URANIUM_INGOT = ITEMS.register("uranium_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
-	public static final RegistryObject<Item> ENDERIUM_INGOT = ITEMS.register("enderium_ingot", 
+
+	public static final RegistryObject<Item> ENDERIUM_INGOT = ITEMS.register("enderium_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
-	public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot", 
+
+	public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
-	public static final RegistryObject<Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot", 
+
+	public static final RegistryObject<Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
-	public static final RegistryObject<Item> ARDITE_INGOT = ITEMS.register("ardite_ingot", 
+
+	public static final RegistryObject<Item> ARDITE_INGOT = ITEMS.register("ardite_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
-	public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot", 
+
+	public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
-	public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", 
+
+	public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
-	public static final RegistryObject<Item> MANYULLYN_INGOT = ITEMS.register("manyullyn_ingot", 
+
+	public static final RegistryObject<Item> MANYULLYN_INGOT = ITEMS.register("manyullyn_ingot",
 			() -> new Item(new Item.Properties().group(WKOMItemGroup.WKOM)));
+
+	public static final RegistryObject<Item> COPPER_SWORD = ITEMS.register("copper_sword",
+			() -> new SwordItem(CustomToolMaterial.COPPER_TOOL, 6, 1.6f,
+					new Item.Properties().group(WKOMItemGroup.WKOM)));
+
+	public static final RegistryObject<Item> COPPER_PICKAXE = ITEMS.register("copper_pickaxe",
+			() -> new SwordItem(CustomToolMaterial.COPPER_TOOL, 4, 1.2f,
+					new Item.Properties().group(WKOMItemGroup.WKOM)));
+
+	public static final RegistryObject<Item> COPPER_AXE = ITEMS.register("copper_axe",
+			() -> new SwordItem(CustomToolMaterial.COPPER_TOOL, 9, 0.9f,
+					new Item.Properties().group(WKOMItemGroup.WKOM)));
+
+	public static final RegistryObject<Item> COPPER_SHOVEL = ITEMS.register("copper_shovel",
+			() -> new SwordItem(CustomToolMaterial.COPPER_TOOL, 4, 1f,
+					new Item.Properties().group(WKOMItemGroup.WKOM)));
+
+	public static final RegistryObject<Item> COPPER_HOE = ITEMS.register("copper_hoe",
+			() -> new SwordItem(CustomToolMaterial.COPPER_TOOL, 1, 3f,
+					new Item.Properties().group(WKOMItemGroup.WKOM)));
+
+	public static final RegistryObject<Item> COPPER_HELMET = ITEMS.register("copper_helmet",
+			() -> new ArmorItem(CustomArmorMaterial.COPPER_ARMOR, null, null));
 }
