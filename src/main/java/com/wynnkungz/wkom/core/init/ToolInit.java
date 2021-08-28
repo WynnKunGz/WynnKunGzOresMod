e1859e0002b110b3a4e5a09ca7584758825828b2
WynnKunGz <wynn.chanakant@gmail.com>
2021-08-28 19:54:47 +0700

    Update ToolInit.java

--- src/main/java/com/wynnkungz/wkom/core/init/ToolInit.java
+++ src/main/java/com/wynnkungz/wkom/core/init/ToolInit.java
@@ -2,7 +2,7 @@ package com.wynnkungz.wkom.core.init;
 
 import com.wynnkungz.wkom.WynnKunGzOresMod;
 import com.wynnkungz.wkom.common.material.CustomToolMaterial;
-import com.wynnkungz.wkom.core.itemgroup.WKOMItemsItemGroup;
+import com.wynnkungz.wkom.core.itemgroup.WKOMToolsItemGroup;
 
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.HoeItem;
@@ -23,22 +23,22 @@ public class ToolInit {
 	
 	public static final RegistryObject<Item> COPPER_SWORD = TOOLS.register("copper_sword",
 			() -> new SwordItem(CustomToolMaterial.COPPER_TOOL, 3, -2.4f,
-					new Item.Properties().group(WKOMItemsItemGroup.ITEMS)));
+					new Item.Properties().group(WKOMToolsItemGroup.TOOLS)));
 
 	public static final RegistryObject<Item> COPPER_PICKAXE = TOOLS.register("copper_pickaxe",
 			() -> new PickaxeItem(CustomToolMaterial.COPPER_TOOL, 1, -2.8f,
-					new Item.Properties().group(WKOMItemsItemGroup.ITEMS)));
+					new Item.Properties().group(WKOMToolsItemGroup.TOOLS)));
 
 	public static final RegistryObject<Item> COPPER_AXE = TOOLS.register("copper_axe",
 			() -> new AxeItem(CustomToolMaterial.COPPER_TOOL, 6, -3.1f,
-					new Item.Properties().group(WKOMItemsItemGroup.ITEMS)));
+					new Item.Properties().group(WKOMToolsItemGroup.TOOLS)));
 
 	public static final RegistryObject<Item> COPPER_SHOVEL = TOOLS.register("copper_shovel",
 			() -> new ShovelItem(CustomToolMaterial.COPPER_TOOL, 1, -3f,
-					new Item.Properties().group(WKOMItemsItemGroup.ITEMS)));
+					new Item.Properties().group(WKOMToolsItemGroup.TOOLS)));
 
 	public static final RegistryObject<Item> COPPER_HOE = TOOLS.register("copper_hoe",
 			() -> new HoeItem(CustomToolMaterial.COPPER_TOOL, -2, -1f, 
-					new Item.Properties().group(WKOMItemsItemGroup.ITEMS)));
+					new Item.Properties().group(WKOMToolsItemGroup.TOOLS)));
 
 }
